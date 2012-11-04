711f8c79511bb39ece7c117abb0486ee9caa696c
Pikachu <pandafulmanda@gmail.com>
2012-11-04 09:23:22 -0600

    load only one style sheet, front page signup

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,11 +1,12 @@
 class ContentController < ApplicationController
-
 	layout "content_layout"
 
 	def index
 		if current_user
 			redirect_to home_path
 		end
+
+
 	end
 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   
   layout "internals"
   before_filter :require_login
-	
+
   def home
     @requests = Request.where(:user_id => current_user.id, :isOpen => true).all 
 	end
